f8f70ca63a890afb4cae6f68317df5b928cd3a2e
TONYCRE8 <tonyingall.business@gmail.com>
2021-10-07 13:48:52 +0100

    add colors auto add

--- api/keycaps/models/keycaps.js
+++ api/keycaps/models/keycaps.js
@@ -1,6 +1,13 @@
 'use strict';
 
- const slugify = require('slugify');
+ const slugify = require('slugify')
+
+ var colors = {
+   red: "#bc251e",
+   blue: "#0084c2",
+   green: "689b34"
+ }
+ const nearestColor = require('nearest-color').from(colors)
 
  module.exports = {
    lifecycles: {
@@ -8,11 +15,22 @@
        if (data.name) {
          data.slug = slugify(data.name.toLowerCase());
        }
+       if (data.colors) {
+        const color = nearestColor(data.colors)
+        const newColor = await strapi.query('keycap-colors').findOne({color: color.name})
+        data.filter_colors.push(newColor)
+      }
      },
      beforeUpdate: async (params, data) => {
        if (data.name) {
          data.slug = slugify(data.name.toLowerCase());
        }
+       if (data.colors)
+       {
+        const color = nearestColor(data.colors)
+        const newColor = await strapi.query('keycap-colors').findOne({color: color.name})
+        data.filter_colors.push(newColor)
+      }
      },
    },
  };
